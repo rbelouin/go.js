787af57d97d08324f92b3671c7328124224d2fba
rbelouin <rodolphe.belouin@gmail.com>
2014-04-23 15:37:26 +0200

    grunt: add jasmine.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,18 @@ module.exports = function(grunt) {
         src: ["dependencies.js", "src/board.js"],
         dest: "go.js"
       }
+    },
+    jasmine: {
+      src: "go.js",
+      options: {
+        specs: "tests/**/*.spec.js"
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-bower-concat");
   grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-jasmine");
 
-  grunt.registerTask("default", ["bower_concat", "concat"]);
+  grunt.registerTask("default", ["bower_concat", "concat", "jasmine"]);
 };
